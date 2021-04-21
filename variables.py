7b51c1d3b94a7c02d0b8c68592f025ed43d58042
anvithareddy <517anureddy@gmail.com>
2021-04-21 22:46:26 +0530

    commit from pycharm in windows env

---
+++ variables.py
@@ -0,0 +1,17 @@
+# variable lets us store a value by assigning it to a name
+user = "James"
+print(user)
+
+# working with variables
+print("\nFEW EXAMPLES")
+x = 7
+print(x)
+print(x+3)
+print(x)
+
+# variables can be reassigned as many times as we want
+print("\nREASSIGNMENT")
+x = 123.456
+print(x)
+x = "This ia a string"
+print(x + "!")
