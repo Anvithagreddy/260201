b2a02e2b118a32abd9406cf09f287febc5252bd6
anvithareddy <517anureddy@gmail.com>
2021-04-21 22:19:55 +0530

    added quotient and reminder commit from pycharm in windows env

--- data_types.py
+++ data_types.py
@@ -6,5 +6,19 @@ print(4 + 1.65)
 
 # exponentiation
 # the result will be float
+print("Exponentiation")
 print(2 ** 5) # (2 power of 5)
 print(9 ** (1/2))
+print(2 ** 2 ** 2)
+
+# Quotient
+# // is used to determine the quotient of a division
+print("Print only quotient")
+print(20 // 6)
+print(40 // 2)
+
+# reminder
+# % is used to get reminder
+print("Print reminder")
+print(20 % 6)
+print(1.25 % .5)
