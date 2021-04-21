aec978cb233d32f8ec187483e992f6f22055ca8f
anvithareddy <517anureddy@gmail.com>
2021-04-21 22:33:09 +0530

    newline and tab commit from pycharm in windows env

--- strings.py
+++ strings.py
@@ -1,3 +1,18 @@
 # create a string by entering text between single or double quotation marks.
 print("Python is fun")
 print('Always look on the bright side')
+
+# backslash
+print('Brain\'s mother: He\'s not an angle.He\'s a very naughty boy!')
+
+# newline and tab
+# \n is used for newline and \t is used for tab space
+print("FOR NEW LINE\n")
+print('one\ntwo\nthree')
+print("""this
+is a 
+multiline
+text""")  # """ is used for multi line
+
+print("\nFOR TAB")
+print("one\ttwo\tthree")
