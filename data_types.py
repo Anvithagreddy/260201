13c9edeed2e59a9a35d64b263965ce4f9501144c
anvithareddy <517anureddy@gmail.com>
2021-04-21 22:12:54 +0530

    first commit from pycharm in windows env

---
+++ data_types.py
@@ -0,0 +1,10 @@
+# floats
+# a float can be added to an integer
+print(8/2)
+print(6 * 7.0)
+print(4 + 1.65)
+
+# exponentiation
+# the result will be float
+print(2 ** 5) # (2 power of 5)
+print(9 ** (1/2))
