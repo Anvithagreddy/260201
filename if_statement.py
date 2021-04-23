fe51f13efe8902e1612f19602cf54e309c2288eb
anvithareddy <517anureddy@gmail.com>
2021-04-23 13:24:58 +0530

    if-within-if commit from pycharm in windows env

--- if_statement.py
+++ if_statement.py
@@ -2,6 +2,16 @@
 # if condition:
 #    statements
 
+print("SIMPLE IF STATEMENT")
 x = 42
 if x > 5:
     print("x is greater than 5")
+
+
+# if within if
+print("\n\nIF WITHIN IF")
+num = 12
+if num > 5:
+    print("Bigger than 5")
+    if num <= 47:
+        print("between 5 and 47")
