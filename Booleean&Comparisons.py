b19e89d4146eb22bdf796bff756d447edbbf3c8f
anvithareddy <517anureddy@gmail.com>
2021-04-23 13:13:51 +0530

    commit from pycharm in windows env

---
+++ Booleean&Comparisons.py
@@ -0,0 +1,16 @@
+# Booleans can have two values: True And False
+print("BOOLEAN")
+my_boolean = True
+print(my_boolean)
+
+print(2 == 3)
+print("hello" == 'hello')
+
+# comparisons operators
+print("COMPARISON OPERATORS")
+x = 7
+print(x != 8)  # not equal to
+print(x > 5)  # greater than
+print(x < 2)  # smaller than
+print(x >= 7)  # greater or equal to
+print(x <= 7)  # smaller or equal to
