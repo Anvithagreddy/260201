2adf7c0c7e1320a78a867c4dbd1b0a35481aa6fa
anvithareddy <517anureddy@gmail.com>
2021-04-25 22:42:57 +0530

    range with third arg(step) commit from pycharm in windows env

--- range.py
+++ range.py
@@ -9,3 +9,8 @@ print("\n\nRANGE WITH TWO ARGS")
 numbers = list(range(3,8))
 print(numbers)
 # it prints from 3 to 7
+
+# there is also third argument with range(), its really useful its called step
+print("\n\nSTEP ARGUMENT")
+numbers = list(range(5,20,2))
+print(numbers)
