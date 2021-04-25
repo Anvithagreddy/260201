8cf761cff45e4c8e25d08d734f7ede6f687ec7c6
anvithareddy <517anureddy@gmail.com>
2021-04-25 22:25:47 +0530

    commit for list operation as lists from pycharm in windows env

---
+++ list-operations.py
@@ -0,0 +1,13 @@
+# list operations
+print("List operations")
+nums = [1,2,3]
+print(nums + [4,5,6])
+print(nums * 3)
+
+# to check if an item is in a particular list, we can use the in operator
+# it returns True if the item occurs one orr more times in the list, and False if iit doesn't
+print("\n\nIN OPERATION")
+words = ["spam", "egg", "spam", "sausage"]
+print("spam" in words)
+print("egg" in words)
+print("tomato" in words)
