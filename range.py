e59390e9a5ba215c9e5e0997d4e96516251af255
anvithareddy <517anureddy@gmail.com>
2021-04-25 22:35:18 +0530

    range with one arg commit from pycharm in windows env

---
+++ range.py
@@ -0,0 +1,5 @@
+# To create number sequences using the range()
+# by default, it starts from 0, increments by 1 and stops brfore the specified number
+print("RANGE")
+numbers = list(range(10))
+print(numbers)
