84e8e24ab213fc5d713e8029efe248dee57f999a
anvithareddy <517anureddy@gmail.com>
2021-04-25 22:29:38 +0530

    anther example commit from pycharm in windows env

--- list-operations.py
+++ list-operations.py
@@ -11,3 +11,10 @@ words = ["spam", "egg", "spam", "sausage"]
 print("spam" in words)
 print("egg" in words)
 print("tomato" in words)
+
+print("\n\n\nANOTHER EXAMPLE")
+nums = [1,2,3]
+print(not 4 in nums)
+print(4 not in nums)
+print(not 3 in nums)
+print(3 not in nums)
