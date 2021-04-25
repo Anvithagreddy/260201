9ddc3169fd7d15aff72cb9d302aaec2928ec63f0
anvithareddy <517anureddy@gmail.com>
2021-04-25 22:01:17 +0530

    initial commit for lists from pycharm in windows env

---
+++ lists.py
@@ -0,0 +1,16 @@
+# lists
+# can create a list by using square brackets with commas separating items
+print("LISTS")
+words = ["Hello", "world", "!"]
+print(words)
+print(words[0])
+print(words[1])
+print(words[2])
+
+print("\n\nexample")
+m=[
+    [1,2,3],
+    [4,5,6]
+]
+print(m)
+print(m[1][2])
