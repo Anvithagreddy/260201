847e0525f3e258157460ed9af176e7e82c567086
anvithareddy <517anureddy@gmail.com>
2021-04-26 20:52:13 +0530

    own function commit from pycharm in windows env

---
+++ own_functions.py
@@ -0,0 +1,15 @@
+print("MAKING OWN FUNCTIONS")
+
+def my_func():
+    print("spam")
+    print("spam")
+    print("spam")
+my_func()
+
+# once we have defined a function, we can call them multiple times in our code
+print("\ncalling multiple times")
+def hello():
+    print("Hello world!")
+hello()
+hello()
+hello()
