36ddd2cc4372f5748cbecc1b55d2263b6487c563
anvithareddy <517anureddy@gmail.com>
2021-04-26 20:28:33 +0530

    insert() and index() commit from pycharm in windows env

--- functions.py
+++ functions.py
@@ -9,4 +9,20 @@ nums = [1, 2, 3]
 nums.append(4)
 print(nums)
 
+# insert() - insert a new item at any position in the list
+print("\n\nUSING INSERT")
+words = ["python","fun"]
+index = 1
+words.insert(index,"is")
+print(words)
+
+# index() - finds the first occurrence of a list item and returns its index
+print("\n\nUSING INDEX")
+letters = ['p','q','r','s','p','u']
+print(letters.index('r'))
+print(letters.index('s'))
+print(letters.index('u'))
+# print(letters.index('z')) gives error
+
+
 
