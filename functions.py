392b72940e20d3d604f3c0544ee024b23d843520
anvithareddy <517anureddy@gmail.com>
2021-04-26 20:07:29 +0530

    len and append commit from pycharm in windows env

---
+++ functions.py
@@ -0,0 +1,12 @@
+# python has a bunch of useful built-in functions
+# one of then len()
+print("\nUSING LEN()")
+nums = [1, 3, 5, 2, 4]
+print(len(nums))
+
+print("\n\nUSING APPEND")
+nums = [1, 2, 3]
+nums.append(4)
+print(nums)
+
+
